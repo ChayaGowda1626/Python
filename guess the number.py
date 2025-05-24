3cc9213dc3c90c1b0c37b4c7b9299d623c7807e8
ChayaGowda1626 <chaya.24.beai@acharya.ac.in>
2025-05-24 21:30:39 +0530

    Add files via upload

---
+++ guess the number.py
@@ -0,0 +1,22 @@
+"""In this program, I am telling  all about myself"""
+def details():#function defination
+    print("""My name is Chaya G B.
+    I am from Karnataka,India.
+    I love reading.
+    And my area of interest is data analytic.""")
+detail()#function calling
+
+
+name=input("enter the name")
+age=input("Enter your age")
+print("MY name is"+name+"I am "+age+"years old")
+
+
+name=input("enter the name")
+age=input("Enter your age")
+print("MY name is %s I am %s years old".%s(name,age)
+
+
+name=input("enter the name")
+age=input("Enter your age")
+print(f"MY name is {name}I am {age} years old")
