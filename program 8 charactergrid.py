0d663827b8ba4214b7ab9b15cc96f7d64bc9fac4
ChayaGowda1626 <chaya.24.beai@acharya.ac.in>
2025-05-24 22:18:26 +0530

    Add files via upload

---
+++ program 8 charactergrid.py
@@ -0,0 +1,16 @@
+#charactergrid
+print("Chaya G B, USN:-1AY24AI024,Sec:-M")
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for y in range(len(grid[0])):
+    for x in range(len(grid)):
+        print(grid[x][y], end='')
+    print()
